cab2517e808e54708440c525eb707b6f549ac5bb
Tony <nhat.phan@ntworld.net>
2018-06-29 19:32:00 +0700

    Add Record - the base class handle data object for knex/mongodb

---
+++ dist/lib/features/Record.d.ts
@@ -0,0 +1,10 @@
+export declare class Record {
+    protected data: object;
+    protected modified: string[];
+    constructor(data?: object | Record);
+    getAttribute<T>(path: string): T;
+    setAttribute<T>(path: string, value: T): boolean;
+    getModified(): string[];
+    markModified(name: string): void;
+    toObject(): object;
+}
